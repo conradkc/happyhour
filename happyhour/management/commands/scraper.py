b2af38cd3b1cad8729f0c50327548f71c0ba8b37
conradkc <konachong@gmail.com>
2018-05-11 14:28:29 -1000

    updated scraping

--- happyhour/management/commands/scraper.py
+++ happyhour/management/commands/scraper.py
@@ -1,16 +1,23 @@
 from django.core.management.base import BaseCommand, CommandError
-import argparse
-import io
+
 from bs4 import BeautifulSoup
-import urllib3
+from urllib.request import Request, urlopen
+from .models import Restaurant
+from django.utils import timezone
 
 class Command(BaseCommand):
     help = 'converts image to text'
 
     def handle(self, *args, **options):
-        url = 'https://www.yelp.com/search?find_desc=Happy+Hour&find_loc=Honolulu,+HI&start=0'
-        http = urllib3.PoolManager()
-        page = http.request('GET', url)
-        soup = BeautifulSoup(page, "html.parser")
-        place_name = soup.find_all('span')
-        print(place_name)
+        for x in range(0,30):
+            start = str(x*10)
+            html = urlopen('https://www.yelp.com/search?find_desc=Happy+Hour&find_loc=Honolulu,+HI&start=' + start)
+            webpage = BeautifulSoup(html.read())
+            names = webpage.find_all('a', class_='biz-name js-analytics-click')
+            addresses = webpage.find_all('div', class_='secondary-attributes')
+            for x in range(len(names)):
+                restaurant = Restaurant.objects.get_or_create(
+                    pub_date = timezone.now(),
+                    name = names[x],
+                )
+
